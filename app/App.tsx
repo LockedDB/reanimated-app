bbea96bd54f5f01853661f939c4048e05a3661de
Daniel Benet <daniel.benet@manomano.com>
2022-07-28 15:49:52 +0200

    Add new screen transitions

--- app/App.tsx
+++ app/App.tsx
@@ -1,17 +1,19 @@
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { PanGestureScreen } from "./screens/PanGesture/PanGestureScreen";
-import {HomeScreen} from "./screens/HomeScreen";
+import { HomeScreen } from "./screens/HomeScreen";
+import { TransitionsScreen } from "./screens/Transitions/TransitionsScreen";
 
 export default function App() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName={"Home screen"}>
-                <Stack.Screen name={"Home screen"} component={HomeScreen}/>
-                <Stack.Screen name={"Pan gesture"} component={PanGestureScreen}/>
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+	return (
+		<NavigationContainer>
+			<Stack.Navigator initialRouteName={"Home screen"}>
+				<Stack.Screen name={"Home screen"} component={HomeScreen} />
+				<Stack.Screen name={"Pan gesture"} component={PanGestureScreen} />
+				<Stack.Screen name={"Transitions"} component={TransitionsScreen} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 }
 
 const Stack = createNativeStackNavigator();
--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -1,25 +1,36 @@
-import {Button, StyleSheet, Text, View} from "react-native";
-import {StatusBar} from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
 export const HomeScreen = ({ navigation }) => {
-    return <View style={styles.container}>
-        <Text>Click to access type of animation</Text>
-        <StatusBar style="auto"/>
-        <View style={styles.buttonsContainer}>
-            <Button title={'Pan Gesture'} color={'black'} onPress={() => navigation.push('Pan gesture')}/>
-        </View>
-    </View>
-}
+	return (
+		<View style={styles.container}>
+			<Text>Click to access type of animation</Text>
+			<StatusBar style="auto" />
+			<View style={styles.buttonsContainer}>
+				<Button
+					title={"Pan Gesture"}
+					color={"black"}
+					onPress={() => navigation.push("Pan gesture")}
+				/>
+				<Button
+					title={"Transitions"}
+					color={"blue"}
+					onPress={() => navigation.push("Transitions")}
+				/>
+			</View>
+		</View>
+	);
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    buttonsContainer: {
-        marginTop: 15,
-        flexDirection: 'row',
-    }
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	buttonsContainer: {
+		marginTop: 15,
+		flexDirection: "row",
+	},
 });
